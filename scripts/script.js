25dccdd7a13bc9b2ef58c3ac85d68a8e73a41a51
Gitesh <giteshsutar88@gmail.com>
2022-12-12 18:00:01 +0530

    updated

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+function Shownav(){
+    document.getElementById("mob-links").style.display="flex";
+}
+function Closenav(){
+    document.getElementById("mob-links").style.display="none";
+}
+
+function redirect(url){
+    window.open(url);
+}
+function download(url){
+    window.location.href=url;
+}
