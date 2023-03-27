1e27a77ef7e1b0a4764dd3d5e7dce8e07b719277
Gitesh <giteshsutar88@gmail.com>
2023-03-28 01:48:27 +0530

    updated

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,12 @@ function Closenav(){
     document.getElementById("mob-links").style.display="none";
 }
 
+function Closenav1(){
+    Closenav()
+    window.location.href='https://drive.google.com/u/0/uc?id=1r7NUifshN3IWZ3_a_tDkj8rz72DL4zRC&export=download';
+    window.open('https://drive.google.com/file/d/1r7NUifshN3IWZ3_a_tDkj8rz72DL4zRC/view');;
+}
+
 function redirect(url){
     window.open(url);
 }
