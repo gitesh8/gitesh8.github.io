80a45e288884485f66ef763b75276bafd0f8217f
Gitesh Sutar <giteshsutar88@gmail.com>
2023-12-13 13:51:33 +0530

    resume updated

--- scripts/script.js
+++ scripts/script.js
@@ -7,13 +7,13 @@ function Closenav(){
 
 function Closenav1(){
     Closenav()
-    window.location.href='https://drive.google.com/u/0/uc?id=1r7NUifshN3IWZ3_a_tDkj8rz72DL4zRC&export=download';
-    window.open('https://drive.google.com/file/d/1r7NUifshN3IWZ3_a_tDkj8rz72DL4zRC/view');;
+    window.location.href='https://drive.google.com/u/0/uc?id=1xuZCvUudFWNYaa14yEYEqMCAJyGX4C-u&export=download';
+    window.open('https://drive.google.com/file/d/1xuZCvUudFWNYaa14yEYEqMCAJyGX4C-u/view?usp=sharing');;
 }
 
 function redirect(url){
     window.open(url);
 }
 function resume(){
-    window.open('https://drive.google.com/file/d/1r7NUifshN3IWZ3_a_tDkj8rz72DL4zRC/view');
+    window.open('https://drive.google.com/file/d/1xuZCvUudFWNYaa14yEYEqMCAJyGX4C-u/view?usp=sharing');
 }
