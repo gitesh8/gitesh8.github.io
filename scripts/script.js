f183a6558e7473fa786afdac42279a1f683e697a
Gitesh Sutar <giteshsutar88@gmail.com>
2024-05-22 16:51:13 +0530

    resume updated

--- scripts/script.js
+++ scripts/script.js
@@ -7,8 +7,8 @@ function Closenav(){
 
 function Closenav1(){
     Closenav()
-    window.location.href='https://drive.google.com/u/0/uc?id=13EuhAGwhTA008-PqJr3KuhhULC9VTJ3N&export=download';
     window.open('https://drive.google.com/file/d/13EuhAGwhTA008-PqJr3KuhhULC9VTJ3N/view?usp=sharing');;
+    window.location.href='https://drive.google.com/u/0/uc?id=13EuhAGwhTA008-PqJr3KuhhULC9VTJ3N&export=download';
 }
 
 function redirect(url){
